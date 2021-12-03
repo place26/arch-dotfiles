3957c7490421db5cefcbd419f3a9109cf74c3a06
twentysix <place.26@daum.net>
2021-12-03 23:11:12 +0000

    neovim headless plugin install

--- install.sh
+++ install.sh
@@ -36,6 +36,9 @@ sudo chsh -s $(which zsh) $USER
 # bundle zsh plugins
 antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
 
+# install neovim plugins
+nvim --headless +PlugInstall +qall
+
 # install tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 tmux source ~/.tmux.conf
