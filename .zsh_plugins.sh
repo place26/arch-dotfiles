61ea1f1fc2505ec3b63f1f20bff8216e9d547160
twentysix <place.26@daum.net>
2021-12-03 23:05:49 +0000

    add .zsh_plugins.sh

---
+++ .zsh_plugins.sh
@@ -0,0 +1,12 @@
+source /home/xupei/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/colored-man-pages/colored-man-pages.plugin.zsh
+fpath+=( /home/xupei/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/colored-man-pages )
+source /home/xupei/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/command-not-found/command-not-found.plugin.zsh
+fpath+=( /home/xupei/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/command-not-found )
+source /home/xupei/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-rupa-SLASH-z/z.sh
+fpath+=( /home/xupei/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-rupa-SLASH-z )
+source /home/xupei/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /home/xupei/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /home/xupei/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /home/xupei/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
+source /home/xupei/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fpath+=( /home/xupei/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
--- install.sh
+++ install.sh
@@ -2,11 +2,12 @@
 curl -L https://nixos.org/nix/install | sh
 
 # source nix
-~/.nix-profile/etc/profile.d/nix.sh
+. ~/.nix-profile/etc/profile.d/nix.sh
 
 # install packages
 nix-env -iA \
 	nixpkgs.zsh \
+	nixpkgs.which \
 	nixpkgs.antibody \
 	nixpkgs.git \
 	nixpkgs.neovim \
